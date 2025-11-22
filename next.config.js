20551d99d79277efeeabf08cd5609d1191ca9bc6
seth-dev-fs <242302371+seth-dev-fs@users.noreply.github.com>
2025-11-22 17:07:27 +0000

    feat: Configure Next/Image remotePatterns and optimization settings

--- next.config.js
+++ next.config.js
@@ -6,13 +6,133 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
-        pathname: '/**', // This allows any image from picsum.photos
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'i.ytimg.com',
-      }
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.gizmodo.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static01.nyt.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.macrumors.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.vox-cdn.com',
+        port: '',
+        pathname: '/**',
+      },
+      // Wildcard domains
+      {
+        protocol: 'https',
+        hostname: '*.theverge.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.techcrunch.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.wired.com',
+        port: '',
+        pathname: '/**',
+      },
+      // Common domains found during scraping
+      {
+        protocol: 'https',
+        hostname: 'cdn.mos.cms.futurecdn.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.sammobile.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.androidauthority.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.gsmarena.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.notebookcheck.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'feeds.weblogssl.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '9to5linux.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'itsfoss.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.omgubuntu.co.uk',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'arstechnica.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
+    unoptimized: false,
+  },
+  experimental: {
+    allowFutureImageOptimization: true, // If applicable to your Next.js version
   },
 };
 
